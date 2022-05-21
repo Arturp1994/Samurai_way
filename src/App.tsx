af0ec9d4308f107cc5f854453c86d735c0314935
Arthur <arturiopr1994@gmail.com>
2022-05-21 13:18:16 +0300

    1

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      HELLO!!!! Arthurdgdfege
+        egege
+        eggeg
+        hhhh
+        rgrgrg
+    </div>
+  );
+}
+
+
+export default App;
