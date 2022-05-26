597257f1aa0f9b8b432d4f570bd96f7f0898a65f
Arthur <arturiopr1994@gmail.com>
2022-05-26 17:07:47 +0300

    React 14 lesson

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import s from './Header.module.css'
+
+const Header = () => {
+    return (
+        <header className={s.header}>
+            <img src ='https://i.pinimg.com/originals/cc/7a/d3/cc7ad3d3ba4e80853304bee2dc3015da.png'/>
+        </header>
+
+    )
+}
+
+export default Header
